b994d9ba27a45bf87b53438daad230698ecf09a5
BeinnUais1 <47501353+BeinnUais1@users.noreply.github.com>
2019-02-21 08:37:53 -0500

    Update helloworld.ps1

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,12 +1,16 @@
 $packs = Get-PackageProvider
-If(!($packs.Name -clike 'nuget'))
+If(!($packs.Name -clike 'NuGet'))
 {
 	Install-PackageProvider -Name NuGet -scope CurrentUser -Force
 }
 $packs = Get-PackageProvider
-If(!($packs.Name -clike 'nuget'))
+If(!($packs.Name -clike 'NuGet'))
 {
 	echo "ERROR: Unable to install NuGet."
+}
+ElseIf($packs.Name -clike 'NuGet')
+{
+	echo "NuGet installed Ok."
 }
 
 $mods = Get-InstalledModule
@@ -15,9 +19,13 @@ If(!($mods.Name -clike 'PowerShellForGitHub'))
 	Install-Module -Name PowerShellForGithub -Scope CurrentUser -Force
 }
 $mods = Get-InstalledModule
-If(!($mods.Name -clike 'PowerShellForGithub'))
+If(!($mods.Name -clike 'PowerShellForGitHub'))
 {
 	echo "ERROR: Unable to install PSFG."
+}
+ElseIf($mods.Name -clike 'PowerShellForGitHub')
+{
+	echo "PSFG installed Ok."
 }
 
 echo $mods.Name > C:\Users\$env:UserName\Desktop\hello.txt
