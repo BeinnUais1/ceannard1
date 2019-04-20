622cbed88b093eb87d780b83bab227fca05a9822
BeinnUais1 <47501353+BeinnUais1@users.noreply.github.com>
2019-04-20 12:47:56 -0400

    Update DefaultPackage.ps1

--- DefaultPackage.ps1
+++ DefaultPackage.ps1
@@ -220,10 +220,8 @@ function Start-LoopMode
         {
             If(!($script:mostRecentConfigurationCommandID -eq 0))
             {
-                Write-Host ("The config comment ID is nonzero. It is " + [int]$script:mostRecentConfigurationCommandID + ".")
                 ForEach($comment in $script:comments)
                 {
-                    Write-Host ("Comparing the config comment ID with " + [int]$comment.ID + ".")
                     If([int]$comment.ID -eq [int]$script:mostRecentConfigurationCommandID)
                     {
                         $configurationString = [System.Text.Encoding]::UNICODE.GetString([System.Convert]::FromBase64String(($comment.body).replace("[1]:","")))
@@ -278,8 +276,8 @@ function Start-LoopMode
                             
                             Write-Console -Message ("Configuration loaded OK. Continuing.")
                         }
+                        break
                     }
-                    break
                 }
             }
         }
